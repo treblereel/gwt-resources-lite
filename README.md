fd89aa6f0f69b7e51109051cd685061ab9508027
Dmitii Tikhomirov <chani.liet@gmail.com>
2022-01-05 15:46:56 -0800

    initial apt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # gwt-resources-lite
 
-This project provides ability to wrap our resources (js, txt, css or images) into generated java classes, so it can be obtained on the client side. Unlike GWT2 resources module, it supports very limited scope of features (no less/gss optimisation, i18n, server-side resources) and has no deps on gwt2.  
+This project provides ability to wrap resources (js, txt, css or images) into generated java classes, so then it can be obtained on the client side. Unlike GWT2 resources module, gwt-resources-lite supports very limited scope of features (no less/gss optimisation, i18n, server-side resources) and has no deps on gwt2.  
 
---
+++ processor/src/main/java/org/treblereel/gwt/resources/apt/ClientBundleAnnotationProcessor.java
@@ -0,0 +1,53 @@
+/*
+ *
+ * Copyright © ${year} ${name}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.treblereel.gwt.resources.apt;
+
+import com.google.auto.service.AutoService;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+/** @author Dmitrii Tikhomirov Created by treblereel on 9/30/18. */
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({"org.treblereel.gwt.resources.api.client.Resource"})
+public class ClientBundleAnnotationProcessor extends AbstractProcessor {
+
+  @Override
+  public boolean process(
+      Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+    if (annotations.isEmpty()) {
+      return false;
+    }
+
+    /*    AptContext context = new AptContext(processingEnv, roundEnvironment);
+    TreeLogger logger = new PrintWriterTreeLogger();
+    ((PrintWriterTreeLogger) logger).setMaxDetail(TreeLogger.Type.INFO);
+    Set<TypeElement> elements = context.getClassesWithAnnotation(Resource.class);
+    try {
+      new ClientBundleClassBuilder(logger, context, elements).process();
+    } catch (UnableToCompleteException e) {
+      throw new Error(e);
+    }*/
+    return true;
+  }
+}
